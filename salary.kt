5e3db5131d4d6a389520c2df7b94f5ec87fc58cc
damconover <123193877+damconover@users.noreply.github.com>
2023-02-11 08:24:51 -0600

    Create salary.kt

---
+++ salary.kt
@@ -0,0 +1,13 @@
+{
+		String name = "Joe Mahoney";
+		int age = 26;
+		double annualPay = 100000;
+		System.out.print("My name is ");
+		System.out.print(name);
+		System.out.print(", my age is ");
+		System.out.print(age);
+		System.out.print(" and I hope to earn $");
+		System.out.print(annualPay);
+		System.out.print(" per year.");
+	}
+	}
